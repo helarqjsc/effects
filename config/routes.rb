542c593546705e35c1e7785190a38fe2e9a8c845
Victor M <gobnetzo@gmail.com>
2015-01-08 14:34:23 +0500

    Some models and stuff

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
-  get '/' => 'home#index'
+  root 'home#index'
+  get '/admin' => 'admin#index', as: 'user_root'
   
+  devise_for :users, 
+    path: '', 
+    path_names: {
+      sign_in: 'login', 
+      sign_out: 'logout'
+    },
+    sign_out_via: ['get']
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
---
+++ db/migrate/20150107152312_create_videos.rb
@@ -0,0 +1,12 @@
+class CreateVideos < ActiveRecord::Migration
+  def change
+    create_table :videos do |t|
+      t.string :title
+      t.string :url
+      t.string :filename
+      t.integer :page_id
+
+      t.timestamps null: false
+    end
+  end
+end
