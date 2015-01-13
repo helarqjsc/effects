b91ab43d94d1c56888e28ba0d8327456f544ca89
Victor M <gobnetzo@gmail.com>
2015-01-13 14:12:24 +0500

    Add new videos

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,18 +12,28 @@ Page.create(title: 'Наведения', slug: 'hovers')
 	.videos.create([                    
 		{url: "http://tympanus.net/Tutorials/CircleHoverEffects/index.html", title: "Circle Hover Effects", filename: "circle.webm"},
 	  {url: "http://tympanus.net/Development/HoverEffectIdeas/index2.html", title: "Hover Effect Ideas. По ссылке больше примеров", filename: "hover2.webm"},
-	  {url: "http://tympanus.net/Tutorials/ShapeHoverEffectSVG/index.html", title: "Shape Hover Effect with SVG", filename: "hover3.webm"}
+	  {url: "http://tympanus.net/Tutorials/ShapeHoverEffectSVG/index.html", title: "Shape Hover Effect with SVG", filename: "hover3.webm"},
+	  {url: "http://tympanus.net/Tutorials/BorderAnimationSVG/", title: "Border Animation SVG", filename: "hoverborder.webm"}   
 ])
 
-Page.create(title: 'Кнопки', slug: 'buttons')
+Page.create(title: 'UI', slug: 'ui')
 	.videos.create([   
-		{url: "http://codepen.io/itsnoureddine/pen/QwEjpK", title: "Buttons menu", filename: "button.webm"}
+		{url: "http://codepen.io/itsnoureddine/pen/QwEjpK", title: "Buttons menu", filename: "button.webm"},
+		{url: "http://tympanus.net/Development/DotNavigationStyles/", title: "Dot Navigation. По ссылке больше примеров", filename: "dot.webm"},
+		{url: "http://pasqualevitiello.github.io/Tumblr-Style-Cog-Spinners/", title: "Tumblr-Style Cog Loader", filename: "loaders.webm"},
+		{url: "http://tympanus.net/Development/SelectInspiration/index.html", title: "Select Box", filename: "select.webm"}
+])
+
+Page.create(title: 'List', slug: 'list')
+	.videos.create([   
+		{url: "http://thecodeplayer.com/walkthrough/grid-animation-effects", title: "Grid animation effects", filename: "3dgrid.webm"}
 ])
 
 Page.create(title: 'Попапы', slug: 'popups')
 	.videos.create([   
 		{url: "http://tristanedwards.me/sweetalert", title: "Sweet Alert", filename: "alert.webm"},
 		{url: "http://tympanus.net/Development/DialogEffects/index.html", title: "Dialog Effects. По ссылке больше примеров", filename: "popups.webm"},
+		{url: "http://tympanus.net/Development/ButtonComponentMorph/index.html", title: "Morphing Buttons. По ссылке больше примеров", filename: "morph.webm"}
 ])
 
 Page.create(title: 'Переходы', slug: 'transitions')
@@ -33,15 +43,19 @@ Page.create(title: 'Переходы', slug: 'transitions')
 		{url: "http://codepen.io/pixelass/pen/gixzc", title: "Apple Mac", filename: "mac.webm"}
 ])
 
-Page.create(title: 'Лоадеры', slug: 'loaders')
-	.videos.create([   
-		{url: "http://pasqualevitiello.github.io/Tumblr-Style-Cog-Spinners/", title: "Tumblr-Style Cog Loader", filename: "loaders.webm"}
-])
-
 Page.create(title: 'Иконки', slug: 'icons')
 	.videos.create([   
 		{url: "http://fian.my.id/marka/", title: "Marka", filename: "icons.webm"}
 ])
 
-Page.create(title: 'Прогресс бары', slug: 'progress-bars')
+Page.create(title: 'Галереи', slug: 'galleries')
+	.videos.create([   
+		{url: "http://photoswipe.com/", title: "Photo Swipe", filename: "photoswipe.webm"}
+])
+
+Page.create(title: 'Параллакс', slug: 'Parallax')
+	.videos.create([   
+		{url: "http://janpaepke.github.io/ScrollMagic/", title: "Scroll Magic", filename: "parallax.webm"}
+])
+
 Page.create(title: 'Табы', slug: 'tabs')
